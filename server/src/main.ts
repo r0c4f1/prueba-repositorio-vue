2902eb52a29be30558d251820e1815aebfc7f1b3
roberto <rducharne2@gmail.com>
2025-05-13 12:06:23 -0400

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "https://prueba-repositorio-vue-e8fg.vercel.app/",
+    origin: "https://prueba-repositorio-vue-e8fg.vercel.app/api",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
